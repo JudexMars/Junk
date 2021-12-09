1ba3a7fc632ffc460e34b05173064033a7665b76
Daniel Brante <36617982+JudexMars@users.noreply.github.com>
2021-12-10 02:05:28 +0300

    Exception

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Func() {
+    bool error = true;
+
+    if (error) {
+        throw string("What?");
+    }
+}
+
+int main()
+{
+    try { Func(); }
+    catch (int e) { cout << "Error code: " << e << endl; }
+    catch (char const* e) { cout << "Error message: " << e << endl; }
+    catch (string &e) { cout << "Error message: " << e << endl; }
+}
