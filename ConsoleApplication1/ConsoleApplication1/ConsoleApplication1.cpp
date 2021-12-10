0a78dd4527aa7e06564280adb73c4cdcc1640cd9
Daniel Brante <36617982+JudexMars@users.noreply.github.com>
2021-12-10 23:11:34 +0300

    Update

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,30 @@
 ﻿#include <iostream>
+#include <exception>
 
 using namespace std;
+
+class MyException : public exception {
+public:
+    virtual const char* what() const throw() {
+        return "Something really bad happened!";
+    }
+};
+
+class Memories {
+public:
+    Memories() {
+        char* pMemory = new char[999];
+        delete[] pMemory;
+        cout << "Memory allocated and then freed of its burden" << endl;
+    }
+};
+
+class Test {
+public:
+    void Halt() {
+        throw MyException();
+    }
+};
 
 void Func() {
     bool error = true;
@@ -10,10 +34,34 @@ void Func() {
     }
 }
 
+void goesWrong() {
+    bool error1 = true;
+    bool error2 = false;
+
+    if (error1) { throw bad_alloc(); }
+    if (error2) { throw exception(); }
+}
+
 int main()
 {
     try { Func(); }
     catch (int e) { cout << "Error code: " << e << endl; }
     catch (char const* e) { cout << "Error message: " << e << endl; }
     catch (string &e) { cout << "Error message: " << e << endl; }
+
+    try {
+        Memories m;
+    }
+    catch (bad_alloc& e) { cout << "Memory CAN NOT be allocated!" << e.what() << endl; }
+
+    
+    Test t;
+    try {
+        t.Halt();
+    }
+    catch (MyException& e) { cout << e.what() << endl; }
+
+    try { goesWrong(); }
+    catch (bad_alloc& e) { cout << "Error: " << e.what() << endl; }
+    catch (exception &e) { cout << "Error message: " << e.what() << endl; }
 }
---
+++ Files/Files/Files.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    //ofstream outFile;
+	fstream outFile;
+	string fname = "text.txt";
+    outFile.open(fname, ios::out);
+
+	if (outFile.is_open())
+	{
+		outFile << "Hello there!" << endl;
+		outFile << "Obiwan Kenobi..." << endl;
+		outFile.close();
+	}
+	else {
+		cout << "File cannot be created: " << fname << endl;
+	}
+}
